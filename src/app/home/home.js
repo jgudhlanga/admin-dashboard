1a0709a7916e005db2b01f3978e08cb233adf437
James Gudhlanga <jimmyneds@gmail.com>
2017-07-22 22:46:59 +0200

    Auth Changes

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,30 @@ import {Component} from '@angular/core';
 })
 
 export class HomeComponent {
+
+  constructor(authService: AuthService, router: Router) {
+    this.router = router;
+    this.authService = authService;
+  }
+
+  user: any;
+  error: any;
+
+  getUser() {
+    if (this.authService.isLoggedIn) {
+      this.authService.getUser()
+        .subscribe(
+          user => {
+            this.user = user;
+          },
+          error => {
+            this.error = error;
+          });
+    }
+  }
+
+  ngOnInit() {
+    this.getUser();
+  }
 
 }

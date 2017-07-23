5d544c9a94085434930c1b3e212c06a385bed3c0
James Gudhlanga <jimmyneds@gmail.com>
2017-07-23 22:43:52 +0200

    Dashboard Updates

--- src/app/dashboard/dashboard.js
+++ src/app/dashboard/dashboard.js
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../auth/auth.service';
 import {EmployeeService} from '../employee/employee.service';
@@ -7,10 +7,12 @@ import {EmployeeService} from '../employee/employee.service';
   selector: 'app-dashboard',
   template: require('./dashboard.html')
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   constructor(router:Router, authService: AuthService, employeeService: EmployeeService) {
     this.router = router;
     this.authService = authService;
     this.employeeService = employeeService;
+    employeeService.getPositionsCount();
   }
+
 }
--- src/app/employee/employee.service.js
+++ src/app/employee/employee.service.js
@@ -20,8 +20,62 @@ export class EmployeeService {
     return this.http.get('http://staging.tangent.tngnt.co/api/employee/', {headers: myHeaders})
       .map((response: Response) => {
         const employees = response.json();
+        if (employees) {
+          localStorage.setItem('allEmployees', JSON.stringify(employees));
+        }
         return employees;
       });
+  }
+
+  getEmployeesCount() {
+    let totalCount = 0;
+    if (localStorage.allEmployees) {
+      totalCount = JSON.parse(localStorage.allEmployees).length;
+    }
+    return totalCount;
+  }
+
+  getFemalesCount() {
+    let femaleCount = 0;
+    if (this.getEmployeesCount() > 0) {
+      const employees = JSON.parse(localStorage.allEmployees);
+      for (let i = 0; i < employees.length; i++) {
+        if (employees[i].gender === 'F') {
+          femaleCount++;
+        }
+      }
+    }
+    return femaleCount;
+  }
+
+  getMalesCount() {
+    let maleCount = 0;
+    if (this.getEmployeesCount() > 0) {
+      const employees = JSON.parse(localStorage.allEmployees);
+      for (let i = 0; i < employees.length; i++) {
+        if (employees[i].gender === 'M') {
+          maleCount++;
+        }
+      }
+    }
+    return maleCount;
+  }
+
+  getPositions() {
+    const positions = {};
+    if (this.getEmployeesCount() > 0) {
+      const employees = JSON.parse(localStorage.allEmployees);
+      for (let i = 0; i < employees.length; i++) {
+        if (employees[i].position.name) {
+          if (Object.prototype.hasOwnProperty.call(positions, employees[i].position.name)) {
+            positions[employees[i].position.name] += 1;
+          } else {
+            Object.assign(positions, {[employees[i].position.name]: 1});
+          }
+        }
+      }
+    }
+    return positions;
   }
 
  }
